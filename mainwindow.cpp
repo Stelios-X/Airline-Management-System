0cb7ac24926ff82114d79bd19deb3dc3bd545320
Robert Angote <ebobby@outlook.com>
2024-12-29 18:25:05 +0300

    Exit button execution functionality

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,6 +8,7 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     connect(ui->pushButton, SIGNAL(clicked()), this,SLOT(on_pushButton_clicked()));
+    connect(ui->pushButton_2,SIGNAL(clicked()),SLOT(close()));
 }
 
 MainWindow::~MainWindow()
